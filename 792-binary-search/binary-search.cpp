4a81ac3c560a0ec672cc983e46947899a744da4b
Alick <144825686+alicko@users.noreply.github.com>
2026-03-13 13:15:08 +0400

    Time: 0 ms (100.00%) | Memory: 31.3 MB (80.29%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -4,7 +4,7 @@ public:
         int left = 0;
         int right = nums.size() - 1;
 
-        while (left <= right) {  // Note: <= not 
+        while (left <= right) {  
             int mid = (left + right) / 2;
 
             if (nums[mid] == target) {
